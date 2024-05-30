2d76b3b03a77ad1b8bf1e89c494e9eed811d6421
Subramanian Muthu kumar <subramanian04051@gmail.com>
2024-05-30 10:58:15 +0530

    Update JwtAuth.js for verifying the jwt token

--- JwtAuth.js
+++ JwtAuth.js
@@ -1,14 +1,16 @@
 const jwt=require('jsonwebtoken')
 const express=require('express')
 const app=express()
-
-const router=express.Router()
+const secret_key="Secret-key"
+const router=express.Router() 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 router.post('/Login',(req,res,next)=>
-{
+{ 
     try{
     var userobj={}
     const secret_key="Secret-key"
-    Object.assign(userobj,req.params.user_Details)
+    Object.assign(userobj,req.body.user_Details)
     const token=jwt.sign(userobj,secret_key,{expiresIn:60*10})
     res.send({status:"SUCCESS",token:token})
     }
@@ -18,8 +20,32 @@ router.post('/Login',(req,res,next)=>
         res.send({status:"SUCCESS",token:null})
     }
 })
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+const TokenVerify=function(req,res,next)
+{
+    const token=req.headers['authorization']
+    //console.log(req.headers) 
+    jwt.verify(token,secret_key,(err,decoed)=>
+    {
+        if(err)
+            {
+                res.send({status:"Failure",data:"SESSION LOGOUT"})
+            } 
+            else{
+                res.send({status:"Success",data:req.body.numbers.first_no+req.body.numbers.second_no})
+            }
+})
+}       
+//app.use(Middleware)
+
+router.post('/Calc',TokenVerify,(req,res,next)=>
+{
+    res.send(req.params.First+req.params.Second)
+    
+               // console.log(decoed)
+      //      }
+    //}) 
+})
+
 app.use(router)
 //module.ex
 
